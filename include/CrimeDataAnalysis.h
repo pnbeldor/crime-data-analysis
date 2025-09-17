b88317a06c11d9bd329fef0db1b2b588111ef7dd
Pierre Nicolson Beldor <pnbeldor@gmail.com>
2025-09-17 14:00:54 -0600

    Added the skeleton project using CMake
    Added a library
    Added unit test
    modified the .gitignore file

---
+++ include/CrimeDataAnalysis.h
@@ -0,0 +1,21 @@
+#ifndef __CRIMEDATAANALYSIS_H__
+#define __CRIMEDATAANALYSIS_H__
+
+#include <iostream>
+
+class CrimeDataAnalysis
+{
+public:
+    CrimeDataAnalysis();
+    ~CrimeDataAnalysis();
+
+void say_hello(){
+    std::cout << "Hello, from crime-analysis!\n";
+}
+
+
+private:
+
+};
+
+#endif  //__CRIMEDATAANALYSIS_H__
